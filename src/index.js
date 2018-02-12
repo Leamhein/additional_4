a1113691f41390442a1b3ad097d21f7edb1adb99
Vladimir Apenko <36323910+Leamhein@users.noreply.github.com>
2018-02-12 23:17:56 +0300

    Add files via upload
    Add some functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  function Reverse (array) {
+   var arr = array.split('');
+   return arr.reverse();
+  };
+  function Multiplication (first, second) {
+    var revResult = [],
+    length = 0;
+    if (first.length > second.length) {
+      length = first.length;
+      revResult = first;
+    } else {
+      length = second.length;
+      revResult = second;
+    };
+    for (var i = 0; i < length; i++) {
+      while (typeOf(first[i]) !== undefined || typeOf(second[i]) !== undefined) {
+        if (first[i] * second[i] > 9) {
+
+        }
+      }
+    }
+  };
+};
+module.exports(1,2);
